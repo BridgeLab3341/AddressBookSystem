b0a8fc196d13d7440b8ea08ae33682a61ee0aac6
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-30 16:05:11 +0530

    [somu]Add:Ability to Edit contact person.

--- AddressBookSystem/AddressBookOperations.cs
+++ AddressBookSystem/AddressBookOperations.cs
@@ -32,7 +32,39 @@ namespace AddressBookSystem
         public void Display()
         {
             {
-                Console.WriteLine("1.First Name: {0}\n2.Last Name: {1}\n3.Address: {2}\n4.City: {3},\n5.Zip: {4}\n6.Phone Number: {5}\n7.Email: {6}",contacts.First_Name,contacts.Last_Name,contacts.Address,contacts.City,contacts.Zip,contacts.Phone_number,contacts.Email);
+                Console.WriteLine("1.First Name=" + contacts.First_Name + "\n2.Last Name=" + contacts.Last_Name + "\n3.Address=" + contacts.Address + "\n4.City=" + contacts.City + "\n5.Zip=" + contacts.Zip + "\n6.Phone Number=" + contacts.Phone_number + "\n7.Email=" + contacts.Email);
+            }
+        }
+        public void EditContacts()
+        {
+            Console.WriteLine("\nChoose option to edit contact\n1.First Name\n2.Last Name\n3.Address\n4.City\n5.State\n6.ZIP\n7.Phone number\n8.E-Mail");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                        Console.WriteLine(contacts.First_Name);
+                    break;
+                case 2:
+                    contacts.Last_Name = Console.ReadLine();
+                    break;
+                case 3:
+                    contacts.Address = Console.ReadLine();
+                    break;
+                case 4:
+                    contacts.City = Console.ReadLine();
+                    break;
+                case 5:
+                    contacts.State = Console.ReadLine();
+                    break;
+                case 6:
+                    contacts.Zip = Console.ReadLine();
+                    break;
+                case 7:
+                    contacts.Phone_number = Console.ReadLine();
+                    break;
+                case 8:
+                    contacts.Email = Console.ReadLine();
+                    break;
             }
         }
     }
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -6,9 +6,27 @@ namespace AddressBookSysytem
     {
         public static void Main(string[] args)
         {
-            AddressBookOperations address=new AddressBookOperations();
-            address.AddContacts();
-            address.Display();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose the Option to Perform the Opearation\n1.Add Contact to the List\n2.Edit Contact List\n3.Exit");
+                int option=Convert.ToInt32(Console.ReadLine());
+                AddressBookOperations address = new AddressBookOperations();
+                switch (option)
+                {
+                    case 1:
+                        address.AddContacts();
+                        address.Display();
+                        break;
+                        case 2:
+                        address.EditContacts();
+                        //address.Display();
+                        break;
+                        case 3:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
